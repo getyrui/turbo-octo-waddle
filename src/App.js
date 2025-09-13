8a55ced61d0f774ad9aead7888d99ae2b93aae2a
getyrui <surelov@inbox.ru>
2025-09-13 14:59:51 +0300

    Create App.js file with casino interface

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Добро пожаловать в казино онлайн!</h1>
+      <p>Начните игру и испытайте удачу.</p>
+      {/* Здесь будет интерфейс казино */}
+    </div>
+  );
+}
+
+export default App;
